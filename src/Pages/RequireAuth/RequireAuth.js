b21ec2181428ff1d19b76e70ca0e6224bf2b1445
swdrana <tctr2s@gmail.com>
2022-04-17 20:16:32 +0600

    checkout page code done

--- src/Pages/RequireAuth/RequireAuth.js
+++ src/Pages/RequireAuth/RequireAuth.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
-const RequireAuth = ({children}) => {
-    const location = useLocation();
-    if(true){
-        return <Navigate to="/login" state={{from:location}} replace></Navigate>
-    } 
-    return children;
+const RequireAuth = ({ children }) => {
+  const location = useLocation();
+  if (false) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
 };
 
 export default RequireAuth;
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
+import Checkout from "../../Pages/Checkout/Checkout";
 import "./Topic.css";
 const Topic = ({ item }) => {
   const { img, id, name, ratings, text } = item;
   const navigate = useNavigate();
+
+  const routeChange = () =>{ 
+    localStorage.setItem('selectedItem', JSON.stringify(item));
+    navigate('/checkout');
+  }
   return (
     <div className="item col-sm-6 col-md-4">
       <div className="single-item p-3 m-1 d-flex flex-column">
@@ -19,7 +25,8 @@ const Topic = ({ item }) => {
         <p className="text-start" title={text}>
           {text > text.slice(0, 200) ? text.slice(0, 200) + " . . ." : text}
         </p>
-        <Button variant="danger" onClick={()=>{navigate('/checkout')}} className="btn-checkout w-25 mx-auto">Select</Button>
+        
+        <Button variant="danger" onClick={(event)=>{routeChange()}} className="btn-checkout w-25 mx-auto">Select</Button>
         {/* <Link to={'/home'}>hmm</Link> */}
       </div>
     </div>
