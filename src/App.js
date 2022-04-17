813fcfe825becc66c62796e92651070c4aa3405b
swdrana <tctr2s@gmail.com>
2022-04-17 14:19:12 +0600

    banner area word finish

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Blogs from "./components/Blogs/Blogs";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import Services from "./components/Services/Services";
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -18,8 +21,11 @@ function App() {
         <Route path="/services" element={<Services/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/blogs" element={<Blogs/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
+// import banner from '../../images/hand-holding-smartphone-with-tutor-screen.jpg'
+import banner from "../../images/Webinar-bro.svg";
+const goToSignUp = () => {};
 const Home = () => {
-    return (
-        <div>
-            this is home
+  let navigate = useNavigate();
+  return (
+    <>
+      <div className="row banner">
+        <div className="col-md-6 px-5 py-5 d-flex align-items-center ">
+          <div className="">
+            <h1 className="text-start">
+              Learn with <span className="orange">Rana</span> anytime anywhere
+            </h1>
+            <h4 className="text-start py-5  pe-5">
+              Our mision is to help people to find the best course online and
+              learn with expert anytime, anywhere.
+            </h4>
+            <Button
+              className=" btn-danger"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              Create Account
+            </Button>
+          </div>
         </div>
-    );
+        <div className="col-md-6  banner-pik d-flex justify-content-center align-items-center">
+          <img src={banner} className="image-fluid" alt="" height="500px"/>
+          {/* <img src="https://e-tutor-html.netlify.app/assets/images/hero-thumb.jpg" className="img-fluid" alt="" /> */}
+        </div>
+      </div>
+      <div className="catagory container my-5 py-3">
+        <h1 className="text-center ">Browse with top category</h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
