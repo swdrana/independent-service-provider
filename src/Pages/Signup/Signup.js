35cab8784d233e0a0157a8a7454c9a2b92faf53e
swdrana <tctr2s@gmail.com>
2022-04-18 12:58:47 +0600

    env variable added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -4,35 +4,49 @@ import "./Signup.css";
 import google from "../../images/g-logo.png";
 const Signup = () => {
   const [isChacked, setIsChacked] = useState(false);
-  console.log(isChacked);
+//   console.log(isChacked);
+  const handelForm = (e)=>{
+      e.preventDefault();
+      const email =e.target.email.value;
+      const password =e.target.password.value;
+      const conformPassword =e.target.conformPassword.value;
+      console.log(email, password, conformPassword);
+  }
   return (
     <div className="d-flex justify-content-center ">
       <div
         className="w-25 d-flex flex-column justify-content-center "
         style={{ height: "80vh" }}
       >
-        <Form className="my-3 ">
+        <Form className="my-3 " onSubmit={handelForm}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" name="password" placeholder="Password" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="conformPassword">
-            <Form.Control type="password" placeholder="Conform Password" />
+            <Form.Control type="password" name="conformPassword" placeholder="Conform Password" />
           </Form.Group>
 
           <Form.Group className="mb-3">
-          <Form.Check
-            type="checkbox"
-            id="disabledFieldsetCheck"
-            label="Accept Terms & Condition"
-            onClick={()=>{setIsChacked(!isChacked)}}
+            <Form.Check
+              type="checkbox"
+              id="disabledFieldsetCheck"
+              label="Accept Terms & Condition"
+              onClick={() => {
+                setIsChacked(!isChacked);
+              }}
+            />
+          </Form.Group>
+          <input
+            className="mx-auto w-50 d-block btn btn-primary"
+            type="submit"
+            value="Sign Up"
+            disabled={!isChacked}
           />
-        </Form.Group>
-          <input className="mx-auto w-50 d-block btn btn-primary" type="submit" value="Sign Up" disabled={!isChacked}/>
         </Form>
 
         <div className="separator d-flex align-items-center">
