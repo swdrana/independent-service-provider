213bf6ca4dc2c049d3be45823912761245de8b0a
swdrana <tctr2s@gmail.com>
2022-04-18 22:00:30 +0600

    JSON File Updated with img

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Checkout from "../../Pages/Checkout/Checkout";
 import "./Topic.css";
 const Topic = ({ item }) => {
-  const { img, id, name, ratings, text } = item;
+  const { img, id, name, prices, text } = item;
   const navigate = useNavigate();
 
   const routeChange = () =>{ 
@@ -15,15 +15,15 @@ const Topic = ({ item }) => {
     <div className="item col-sm-6 col-md-4">
       <div className="single-item p-3 m-1 d-flex flex-column">
         <img src={img} alt="" height={200} className="rounded-3"/>
-        <div className="ms-4">
-          <h5 className="text-start m-0 p-0">{name}</h5>
+        <div className="mt-2">
+          <h3 className="text-start m-0 p-0">{name}</h3>
           <p className="text-start m-0 mt-1 p-0">
-            Rating: {ratings.toFixed(2)}{" "}
+            Price: {prices}{" "}
           </p>
         </div>
         <hr />
         <p className="text-start" title={text}>
-          {text > text.slice(0, 200) ? text.slice(0, 200) + " . . ." : text}
+          {text > text.slice(0, 80) ? text.slice(0, 80) + " . . ." : text}
         </p>
         
         <Button variant="danger" onClick={(event)=>{routeChange()}} className="btn-checkout w-25 mx-auto">Select</Button>
