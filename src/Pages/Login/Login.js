fa57c9d6cfb641d576d2c253bfa76e0f2092ff25
swdrana <tctr2s@gmail.com>
2022-04-18 12:38:41 +0600

    sign in, up added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './Login.css'
+import google from "../../images/g-logo.png";
 const Login = () => {
     return (
         <div>
-            <h2>Login</h2>
+            
+    <div className="d-flex justify-content-center ">
+      <div
+        className="w-25 d-flex flex-column justify-content-center "
+        style={{ height: "80vh" }}
+      >
+        <Form className="my-3 ">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          {/* <Form.Group className="mb-3">
+          <Form.Check
+            type="checkbox"
+            id="disabledFieldsetCheck"
+            label="Already account?"
+          />
+        </Form.Group> */}
+          <Button className="py-2 mt-4 w-50 mx-auto d-block" type="submit">
+            Sign In
+          </Button>
+        </Form>
+
+        <div className="separator d-flex align-items-center">
+          <div className="w-100 line mx-3"></div>
+          <span className="orange">Or</span>
+          <div className="w-100 line mx-3"></div>
+        </div>
+
+        <div className="pop-up-authorize my-3 d-flex justify-content-center">
+          <Button>
+            <img src={google} height={40} className="rounded-3 me-3" alt="" />
+            <span>Sign in with Google</span>
+          </Button>
+        </div>
+      </div>
+    </div>
         </div>
     );
 };
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import './Signup.css'
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "./Signup.css";
+import google from "../../images/g-logo.png";
 const Signup = () => {
-    return (
-        <div>
-            <h1>Signup</h1>
+  const [isChacked, setIsChacked] = useState(false);
+  console.log(isChacked);
+  return (
+    <div className="d-flex justify-content-center ">
+      <div
+        className="w-25 d-flex flex-column justify-content-center "
+        style={{ height: "80vh" }}
+      >
+        <Form className="my-3 ">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="conformPassword">
+            <Form.Control type="password" placeholder="Conform Password" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+          <Form.Check
+            type="checkbox"
+            id="disabledFieldsetCheck"
+            label="Accept Terms & Condition"
+            onClick={()=>{setIsChacked(!isChacked)}}
+          />
+        </Form.Group>
+          <input className="mx-auto w-50 d-block btn btn-primary" type="submit" value="Sign Up" disabled={!isChacked}/>
+        </Form>
+
+        <div className="separator d-flex align-items-center">
+          <div className="w-100 line mx-3"></div>
+          <span className="orange">Or</span>
+          <div className="w-100 line mx-3"></div>
         </div>
-    );
+
+        <div className="pop-up-authorize my-3 d-flex justify-content-center">
+          <Button>
+            <img src={google} height={40} className="rounded-3 me-3" alt="" />
+            <span>Sign up with Google</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
